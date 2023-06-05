d567c30fbe95cfb94ffb01527464b39dc2289079
Andres <andres.camargo@correounivalle.edu.co>
2023-06-04 21:38:22 -0500

    corners heuristic added

--- src/HexAgent.js
+++ src/HexAgent.js
@@ -177,29 +177,8 @@ function twoBridgesScore(board, player) {
 
   score = twoBridges/2 - twoBridgesAdversary/2;
 
-  // if (score !== 0) {
-  //   console.log("score: ", score, twoBridges, twoBridgesAdversary);
-  //   console.log(board);
-  //   process.exit();
-  // }
-
   return player === '1' ? score : -score;
 }
-
-  
-let testboard =  [
-  [0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, '1', 0],
-  [0, 0, 0, '1', 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0]
-]
-//console.log(twoBridgesScore(testboard, '1'))
-/*
-
-*/
 
 function minmax(board, profundidad, maxplayer, alfa = Number.MIN_SAFE_INTEGER, beta = Number.MAX_SAFE_INTEGER) {
 
@@ -209,7 +188,7 @@ function minmax(board, profundidad, maxplayer, alfa = Number.MIN_SAFE_INTEGER, b
       return [boardS.boardScore(board, '1'), null]
     }else{
       if(profundidad === 0 || movements.length === 2){
-        return [boardS.boardScore(board, '1')+twoBridgesScore(board, '1'), null]
+        return [boardS.boardScore(board, '1')+twoBridgesScore(board, '1')+evaluateConnectionsInCornersScore(board, '1'), null]
       }
     }
   }else {    
@@ -218,7 +197,7 @@ function minmax(board, profundidad, maxplayer, alfa = Number.MIN_SAFE_INTEGER, b
         return [boardS.boardScore(board, '1'), null]
       }else
         if(profundidad === 0 || movements.length === 2) {
-        return [boardS.boardScore(board, '1')+twoBridgesScore(board, '1'), null];
+        return [boardS.boardScore(board, '1')+twoBridgesScore(board, '1')+evaluateConnectionsInCornersScore(board, '1'), null];
       }
     
   }
@@ -271,7 +250,11 @@ function minmax(board, profundidad, maxplayer, alfa = Number.MIN_SAFE_INTEGER, b
         max_eval = evaluation;
         bestMove = [row, col];
       }
+      // Actualiza el valor de alfa con el máximo entre el valor actual y evaluation.
+
       alfa = Math.max(alfa, evaluation);
+      // Realiza la poda alfa-beta verificando si beta es menor o igual a alfa. Si se cumple, se interrumpe el bucle.
+
       if (beta <= alfa) {
         break;
       }
@@ -280,6 +263,8 @@ function minmax(board, profundidad, maxplayer, alfa = Number.MIN_SAFE_INTEGER, b
   } else {
     let min_eval = Number.POSITIVE_INFINITY;
     let bestMove = null;
+    // Cuando no es el turno del jugador maximizador, se transpone el tablero utilizando la función transposeHex
+    // para intercambiar los roles de los jugadores
     let possibleMoves = boardS.boardPath(transposeHex(board));
     possibleMoves.shift();
     possibleMoves.pop();
@@ -312,7 +297,9 @@ function minmax(board, profundidad, maxplayer, alfa = Number.MIN_SAFE_INTEGER, b
       } catch (error) {}     
     })
     possibleMoves = possibleMoves.concat(twoBridgesCandidates);
-
+    // El ciclo for itera sobre los movimientos disponibles (possibleMoves) y realiza la transposición de cada movimiento.
+    // Este proceso de transposición de movimientos permite que el algoritmo continúe evaluando el tablero correctamente, 
+    // considerando el cambio de roles entre los jugadores.
     possibleMoves.forEach((squareId, index) => {
       let row = Math.floor(squareId / board.length)
       let col = squareId % board.length
@@ -337,4 +324,96 @@ function minmax(board, profundidad, maxplayer, alfa = Number.MIN_SAFE_INTEGER, b
     }    
     return [min_eval, bestMove];
   }
+}
+function evaluateConnectionsInCornersScore(board, player) {
+  const boardSize = board.length;
+
+  // Se define las coordenadas de las esquinas
+  const corners = [
+    [0, 0],
+    [0, boardSize - 1],
+    [boardSize - 1, 0],
+    [boardSize - 1, boardSize - 1]
+  ];
+
+  let score = 0;
+
+  // Se verifica si las piedras del jugador forman conexiones en las esquinas
+  for (const [row, col] of corners) {
+    if (board[row][col] === '1') {
+      // Se verificar si hay una conexión en la esquina actual
+      const isConnected = checkCornerConnection(board, row, col, '1');
+      if (isConnected) {
+        // Se asignar un puntaje más alto si hay una conexión en la esquina
+        score += 100;
+      }
+    }
+  }
+
+  // Se da valor positivo al jugador 1
+  return score;
+}
+
+// Función para verificar si hay una conexión en la esquina dada
+function checkCornerConnection(board, row, col, player) {
+  const boardSize = board.length;
+
+  // Se verifican las conexiones en las direcciones adyacentes a la esquina
+  const directions = [
+    [[0, 1], [1, 0]],  // Derecha e inferior
+    [[1, -1], [1, 0]], // Inferior derecha e inferior
+    [[0, -1], [-1, 0]] // Izquierda y superior
+  ];
+
+  for (const [dir1, dir2] of directions) {
+    let connected = true;
+    let currRow = row;
+    let currCol = col;
+
+    // Se verifica si las piedras forman una conexión en la dirección actual
+    for (let i = 0; i < boardSize; i++) {
+      currRow += dir1[0];
+      currCol += dir1[1];
+
+      // Se verifica los límites del tablero, si no se cume se detiene la iteracion
+      if (currRow < 0 || currRow >= boardSize || currCol < 0 || currCol >= boardSize) {
+        break;
+      }
+
+      if (board[currRow][currCol] !== '1') {
+        connected = false;
+        break;
+      }
+    }
+
+    // Si no se conecta, se continua con la siguiente iteración
+    if (!connected) {
+      continue;
+    }
+
+    // Si si está conectado, se verifica si hay una conexión diagonal en la dirección actual
+    currRow = row;
+    currCol = col;
+
+    for (let i = 0; i < boardSize; i++) {
+      currRow += dir2[0];
+      currCol += dir2[1];
+
+      // Se verifica los límites del tablero
+      if (currRow < 0 || currRow >= boardSize || currCol < 0 || currCol >= boardSize) {
+        break;
+      }
+
+      if (board[currRow][currCol] !== '1') {
+        connected = false;
+        break;
+      }
+    }
+
+    if (connected) {
+      return true;
+    }
+  }
+
+  return false;
 }
